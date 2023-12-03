8cd658eff276153aa04f39f6da9a27d56007e6de
GulizaAitkulova <gulizaaitkulova@gmail.com>
2023-12-03 16:23:19 +0900

    update training hyperparameters

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
 data_path = "Car_License_Plates-1/data.yaml"
-results = model.train(epochs=10, data=data_path, imgsz=640)
+results = model.train(epochs=100, data=data_path, imgsz=640, batch=64, device=0)
