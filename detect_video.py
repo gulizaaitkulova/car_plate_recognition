b16a3a32d2577a4c725db85238e2c2f2fe583c5f
Daniiar Raiymkulov <daniiar.raiymkulov@inha.edu>
2023-12-05 23:33:58 +0900

    edit detect_video.py add showcase

--- detect_video.py
+++ detect_video.py
@@ -1,5 +1,19 @@
 """ Run the demo on given video """
 from ultralytics import YOLO
+import os
 
+video_dir = 'video_showcase_data'
+results_list = []
 model = YOLO('yolov8n_car_plate.pt')
-results = model.predict(source="demo.mp4", show=True)
+# results = model.predict(source="IMG_0861.MOV", show=True)
+
+# Iterate over all images in the directory
+for video_file in os.listdir(video_dir):
+    # Construct full image path
+    video_path = os.path.join(video_dir, video_file)
+
+    # Perform object detection
+    results = model.predict(source=video_path, save=True)
+
+    # Append results to the results list
+    results_list.append(results)
